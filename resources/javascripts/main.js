0e6b60a4a873c793d2b65c73a747d04a4e85ed78
Francisco De La Hoz <franciscodelahoz18@outlook.com>
2019-02-11 21:58:26 -0500

    first commit

---
+++ resources/javascripts/main.js
@@ -0,0 +1,35 @@
+let shouldStop = false;
+const buttonRecord = document.getElementById('recordAudio');
+const buttonStop = document.getElementById('stopRecord');
+
+function handleAudioRecord(stream) {
+	const audioChuncks = [];
+	window.mediaRecorder = new MediaRecorder(stream);
+
+	window.mediaRecorder.start();
+
+	window.mediaRecorder.addEventListener('dataavailable', (event) => {
+		if (event.data.size) {
+			audioChuncks.push(event.data);
+		}
+	});
+
+	window.mediaRecorder.addEventListener('stop', () => {
+		delete window.mediaRecorder;
+		const audioBlob = new Blob(audioChuncks);
+		const audioURL = URL.createObjectURL(audioBlob);
+		const audio = new Audio(audioURL);
+		audio.play();
+	});
+}
+
+buttonRecord.addEventListener('click', () => {
+	navigator.mediaDevices.getUserMedia({ audio: true })
+		.then(handleAudioRecord);
+});
+
+buttonStop.addEventListener('click', () => {
+	if (window.mediaRecorder) {
+		window.mediaRecorder.stop();
+	}
+});
